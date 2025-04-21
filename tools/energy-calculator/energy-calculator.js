1c9faa3e93dc3d0d595e3485f134260b8d167c93
Skybox2011 <skybox2011@protonmail.com>
2025-04-21 07:40:30 -0400

    Rewrite and simplify energy calculator; minor styling adjustments

--- tools/energy-calculator/energy-calculator.js
+++ tools/energy-calculator/energy-calculator.js
@@ -1,89 +1,44 @@
 // var hoursPerKwh = 1000 / wattage;
-// var perHourCost = 1000 / wattage * quantity * energyRate
+// var perHourCost = 1000 / wattage * energyRate
 
 // The inputs that we're getting our data from
-let energyRate = document.getElementById("energyRate");
+let energyRate = document.getElementById("energy-rate");
+let wattage = document.getElementById("wattage");
+let quantity = document.getElementById("quantity");
 
-let dev1Watts = document.getElementById("device1Wattage");
-let dev2Watts = document.getElementById("device2Wattage");
+// Output that we can write to
+let costOutput = document.getElementById("cost-output");
 
-let dev1Qty = document.getElementById("device1Quantity");
-let dev2Qty = document.getElementById("device2Quantity");
+console.log("Energy rate: " + energyRate.value);
+console.log("Wattage: " + wattage.value);
 
-// The outputs where we are displaying our data
-let dev1kwhOutput = document.getElementById("dev1kWhOutput");
-let dev2kwhOutput = document.getElementById("dev2kWhOutput");
-
-let dev1costOutput = document.getElementById("dev1costOutput");
-let dev2costOutput = document.getElementById("dev2costOutput");
-
-function hoursPerKwh(wattage, quantity = 1) {
-  return (1000 / wattage) / quantity;
+function hoursPerKwh(wattage) {
+  return 1000 / wattage;
 }
 
-function costPerHour(energyRate, wattage, quantity = 1) {
-  let kwh = wattage / 1000 * quantity;
-  return energyRate * kwh;
+function wattsToKwh(wattage) {
+  return wattage / 1000;
 }
 
+function costPerHour(energyRate, wattage) {
+  let kwh = wattage / 1000;
+  return (energyRate * kwh).toFixed(6);
+}
 
 // Instead of having to press a calculate button, let's just update the math in real time. Much more convenient that way.
 //
 let updateOutput = function() {
-
-  // Start doing the math and updating the text accordingly
-  
-  let dev1hrsPerKwh = 1000 / dev1Watts.value;
-  let dev2hrsPerKwh = 1000 / dev2Watts.value;
-
-  // Divide the hrs per kwh by quantity in a separate statement. It was acting strange
-  // when doing all the math in a single statement.
-  //
-  // UPDATE: This didn't fix it either. What the hell, man?
-  dev1hrsPerKwh = (dev1hrsPerKwh / dev1Qty.value).toFixed(2);
-  dev2hrsPerKwh = (dev2hrsPerKwh / dev2Qty.value).toFixed(2);
-
-
-  if (dev1hrsPerKwh > dev2hrsPerKwh) {
-    dev1kwhOutput.style.color = "green";
-    dev1kwhOutput.textContent = dev1hrsPerKwh + " hrs/kWh";
-
-    dev2kwhOutput.style.color = "red";
-    dev2kwhOutput.textContent = dev2hrsPerKwh + " hrs/kWh";
-  }
-  else {
-    dev1kwhOutput.style.color = "red";
-    dev1kwhOutput.textContent = dev1hrsPerKwh + " hrs/kWh";
-
-    dev2kwhOutput.style.color = "green";
-    dev2kwhOutput.textContent = dev2hrsPerKwh + " hrs/kWh";
-  }
-
-
-  let dev1costPerHour = costPerHour(energyRate.value, dev1Watts.value, dev1Qty.value).toFixed(5);
-  let dev2costPerHour = costPerHour(energyRate.value, dev2Watts.value, dev2Qty.value).toFixed(5);
-
-  if (dev1costPerHour > dev2costPerHour) {
-    dev1costOutput.style.color = "red";
-    dev2costOutput.style.color = "green";
-  }
-  else {
-    dev1costOutput.style.color = "green";
-    dev2costOutput.style.color = "red";
-  }
-
-  dev1costOutput.textContent = "$" + dev1costPerHour + "/hr";
-  dev2costOutput.textContent = "$" + dev2costPerHour + "/hr";
-
-  console.log("Device 1 cost per hour: " + dev1costPerHour);
-  console.log("Device 2 cost per hour: " + dev2costPerHour);
+  let cost = "$" + (costPerHour(energyRate.value, wattage.value) * quantity.value) + "/hr";
+  console.log("Test is this working?");
+  costOutput.textContent = cost;
 };
 
+// energyRate.addEventListener("input", updateOutput);
 energyRate.addEventListener("input", updateOutput);
-dev1Watts.addEventListener("input", updateOutput);
-dev2Watts.addEventListener("input", updateOutput);
-dev1Qty.addEventListener("input", updateOutput);
-dev2Qty.addEventListener("input", updateOutput);
+
+wattage.addEventListener("input", updateOutput);
+
+quantity.addEventListener("input", updateOutput);
 
 // Run the function at the very end of the script. When the user refreshes the page on some browsers, they won't need to re-enter the input to see the output.
 updateOutput();
